496cf8433b262295956610a7a19003a4ef1b92c1
Wynne-Tran <wynnetran2018@gmail.com>
2022-10-15 19:34:06 -0400

    Done DrawMenu

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
