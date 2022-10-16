b1952952ec54840fe060585ff09ea80a2c5600c0
Wynne-Tran <wynnetran2018@gmail.com>
2022-10-16 00:03:02 -0400

    Pass data between fragment

--- app/src/main/java/com/example/practice/DataFragment.kt
+++ app/src/main/java/com/example/practice/DataFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 class DataFragment : Fragment() {
@@ -17,8 +18,15 @@ class DataFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_data, container, false)
         val button = view.findViewById<Button>(R.id.button)
+        var num = 0
+        view.findViewById<TextView>(R.id.result).apply {
+            text = requireArguments().getInt("itemid").toString()
+            num = requireArguments().getInt("itemid") + 1
+        }
         button.setOnClickListener {
-            findNavController().navigate(R.id.action_dataFragment_to_homeFragment)
+            findNavController().navigate(R.id.action_dataFragment_to_homeFragment, Bundle().apply {
+                putInt("dataId", num)
+            })
         }
         return view
     }
--- app/src/main/java/com/example/practice/HomeFragment.kt
+++ app/src/main/java/com/example/practice/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
@@ -16,8 +17,19 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val button = view.findViewById<Button>(R.id.button)
+        var num = 0
+        view.findViewById<TextView>(R.id.result).apply {
+            text = requireArguments().getInt("dataId").toString()
+            if (text == "0"){
+                num = 0
+            } else {
+                num = requireArguments().getInt("dataId") + 1
+            }
+        }
         button.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_dataFragment)
+            findNavController().navigate(R.id.action_homeFragment_to_dataFragment, Bundle().apply {
+                putInt("itemid", num)
+            })
         }
         return view
     }
